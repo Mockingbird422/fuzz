f08227313f7e935575176308872142cb7da6faa4
Andrew Marder <andrew.n.marder@gmail.com>
2016-12-16 14:09:22 -0500

    Identify bug - cannot load settings file

--- canonical_gazetteer.py
+++ canonical_gazetteer.py
@@ -9,18 +9,16 @@ import exampleIO
 import dedupe
 import os
 import time
-import random
 import optparse
 import logging
-from collections import defaultdict
 
 optp = optparse.OptionParser()
 optp.add_option('-v', '--verbose', dest='verbose', action='count',
                 help='Increase verbosity (specify multiple times for more)'
                 )
 (opts, args) = optp.parse_args()
-log_level = logging.WARNING 
-if opts.verbose :
+log_level = logging.WARNING
+if opts.verbose:
     if opts.verbose == 1:
         log_level = logging.INFO
     elif opts.verbose >= 2:
@@ -29,17 +27,16 @@ logging.getLogger().setLevel(log_level)
 
 # create a random set of training pairs based on known duplicates
 
+
 def canonicalImport(filename):
     preProcess = exampleIO.preProcess
     data_d = {}
- 
+
     with open(filename) as f:
         reader = csv.DictReader(f)
         for i, row in enumerate(reader):
-            clean_row = {k : preProcess(v) for (k, v) in
-                         viewitems(row)}
+            clean_row = {k: preProcess(v) for (k, v) in viewitems(row)}
             data_d[filename + str(i)] = clean_row
-
 
     return data_d, reader.fieldnames
 
@@ -83,26 +80,27 @@ t0 = time.time()
 print('number of known duplicate pairs', len(duplicates_s))
 
 if os.path.exists(settings_file):
-    with open(settings_file, 'rb') as f :
+    with open(settings_file, 'rb') as f:
+        # TODO: loading settings file does not work
         gazetteer = dedupe.StaticGazetteer(f)
 else:
     fields = [{'field': 'name', 'type': 'String'},
               {'field': 'address', 'type': 'String'},
               {'field': 'cuisine', 'type': 'String'},
-              {'field': 'city','type' : 'String'}
+              {'field': 'city', 'type': 'String'}
               ]
 
     gazetteer = dedupe.Gazetteer(fields)
-    gazetteer.sample(data_1, data_2, 10000) 
+    gazetteer.sample(data_1, data_2, 10000)
     gazetteer.markPairs(training_pairs)
     gazetteer.train()
-    
+
 if not gazetteer.blocked_records:
     gazetteer.index(data_2)
 
 with open(settings_file, 'wb') as f:
     gazetteer.writeSettings(f, index=True)
-        
+
 alpha = gazetteer.threshold(data_1)
 
 
@@ -118,4 +116,3 @@ confirm_dupes = set(frozenset(pair)
 evaluateDuplicates(confirm_dupes, duplicates_s)
 
 print('ran in ', time.time() - t0, 'seconds')
-
