b32e907cf8dbb4229aa6603fc765643cb3b24f07
Andrew Marder <andrew.n.marder@gmail.com>
2017-01-03 14:10:04 -0500

    Split train and merge into two steps

--- fuzzy_merge.py
+++ fuzzy_merge.py
@@ -15,73 +15,26 @@ from tqdm import tqdm
 import click
 import json
 import re
-
-
-# https://github.com/datamade/dedupe/blob/master/tests/exampleIO.py#L5-L11
-def _clean(s):
-    result = re.sub('\n', ' ', s)
-    result = re.sub(r'[^\x00-\x7F]','?', result) # remove non-ascii characters
-    result = re.sub('  +', ' ', result)
-    result = result.strip().strip('"').strip("'").lower()
-    if not result:
-        result = None
-    return result
-
-
-def read(path, encoding='utf-8'):
-    data_d = {}
- 
-    with open(path) as f:
-        reader = csv.DictReader(f)
-        for i, row in tqdm(enumerate(reader)):
-            clean_row = {k : _clean(v.decode(encoding)) for (k, v) in row.iteritems()}
-            data_d[i] = clean_row
-
-    return data_d
+from train import read
 
 
 @click.command()
-@click.option('--clean-path', default='example/restaurant-1.csv')
 @click.option('--messy-path', default='example/restaurant-2.csv')
-@click.option('--training-file', default='example/training.json')
 @click.option('--logger-level', default='WARNING')
 @click.option('--num-cores', default=1)
-@click.option('--fields-file', default='example/fields.json')
+@click.option('--settings-file', default='example/my.settings')
 @click.option('--output-file', default='example/output.csv')
-@click.option('--sample-size', default=10000)
-def main(clean_path, messy_path, training_file, logger_level, num_cores, fields_file, output_file, sample_size):
+def main(messy_path, logger_level, num_cores, settings_file, output_file):
     # Set logger level
     log_level = getattr(logging, logger_level)
     logging.getLogger().setLevel(log_level)
 
     logging.info('Reading data ...')
-    clean = read(clean_path)
     messy = read(messy_path, encoding='latin-1')
 
-    logging.info('Reading metadata ...')
-    with open(fields_file) as f:
-        fields = json.load(f)
-
     logging.info('Initializing gazetteer ...')
-    gazetteer = dedupe.Gazetteer(fields, num_cores=num_cores)
-
-    logging.info('Sampling pairs for gazetteer ...')
-    gazetteer.sample(clean, messy, sample_size=sample_size)
-
-    # Train the gazetteer at the console
-    if os.path.exists(training_file):
-        with open(training_file, 'r') as tf:
-            gazetteer.readTraining(tf)
-    dedupe.consoleLabel(gazetteer)
-    # Save the manual entries
-    with open(training_file, 'w') as tf:
-        gazetteer.writeTraining(tf)
-
-    logging.info('Training gazetteer ...')
-    gazetteer.train()
-
-    logging.info('Indexing gazetteer ...')
-    gazetteer.index(clean)
+    with open(settings_file) as f:
+        gazetteer = dedupe.StaticGazetteer(f, num_cores=num_cores)
 
     logging.info('Find matches ...')
     matches = gazetteer.match(messy, threshold=0)
