f5fd7f340928203b8bd010137f35c6d5ea6882d4
Andrew Marder <andrew.n.marder@gmail.com>
2017-01-12 11:50:42 -0500

    Code clean up

--- index.py
+++ index.py
@@ -19,53 +19,41 @@ def line_offsets(path):
             offset += len(line)
 
 
-class CsvFile(object):
+class CsvFile(dict):
 
-    def __init__(self, path):
-        self.path = path
-        self.nrows = nrows(path)
-
-    def to_dict(self):
-        return {
-            'csv_path': self.path,
-            'number_of_rows': self.nrows,            
-        }
-
-    def index(self, nblocks):
+    def index(self, path, nblocks):
         '''
         Create `nblocks` of a csv file. Each block (except the last) has
         ceiling(nrows / nblocks) rows.
         '''
-        self.blocks = {}
-        block_size = int(math.ceil(float(self.nrows) / float(nblocks)))
-        self.block_size = block_size
+        self['path'] = path
+        self['nrows'] = nrows(path)
+        self['nblocks'] = nblocks
+        self['blocks'] = {}
+        self['block_size'] = int(math.ceil(float(self['nrows']) / float(nblocks)))
 
-        template = '%(csv_path)s has %(number_of_rows)s rows. To break it into %(nblocks)s blocks, each block will have %(block_size)s rows.'
-        data = self.to_dict()
-        data.update(locals())
-        logging.info(template % data)
+        template = '%(path)s has %(nrows)s rows. To break it into %(nblocks)s blocks, each block will have %(block_size)s rows.'
+        logging.info(template % self)
 
         first_row_in_block = None
         block_number = 1
-        for row, offset in line_offsets(self.path):
-            if row % block_size == 1:
+        for row, offset in line_offsets(self['path']):
+            if row % self['block_size'] == 1:
                 if first_row_in_block is not None:
-                    assert row - first_row_in_block == block_size
+                    assert row - first_row_in_block == self['block_size']
                 first_row_in_block = row
-                self.blocks[block_number] = {
+                self['blocks'][block_number] = {
                     'first_row_number': row,
                     'offset': offset
                 }
                 block_number += 1
 
-    def write_index(self, path):
-        with open(path, 'w') as f:
-            json.dump({
-                'path': self.path,
-                'nrows': self.nrows,
-                'blocks': self.blocks,
-                'block_size': self.block_size,
-            }, fp=f, indent=2)
+    def dump(self, fp):
+        json.dump(self, fp=fp, indent=2)
+
+    def load(self, fp):
+        data = json.load(fp)
+        self.update(data)
 
 
 @click.command()
@@ -73,9 +61,10 @@ class CsvFile(object):
 @click.option('--nblocks', default=10)
 @click.option('--json-file', default='index.json')
 def index(messy, nblocks, json_file):
-    csv_file = CsvFile(messy)
-    csv_file.index(nblocks)
-    csv_file.write_index(json_file)
+    csv_file = CsvFile()
+    csv_file.index(path=messy, nblocks=nblocks)
+    with open(json_file, 'w') as f:
+        csv_file.dump(f)
 
 
 if __name__ == '__main__':
