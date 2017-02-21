0af6ce71d37ec30743f5042c4d9f1a41486df67c
Andrew Marder <andrew.n.marder@gmail.com>
2017-02-21 16:11:20 -0500

    Install using setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+import re
+
+
+def get_requirements():
+    requirements = []
+    with open('requirements.txt') as f:
+        for line in f:
+            requirement = re.sub('#.*', '', line).strip()
+            if requirement:
+                requirements.append(requirement)
+    return requirements
+
+
+setup(
+    name="fuzz",
+    version="0.1",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=get_requirements(),
+    entry_points='''
+        [console_scripts]
+        fuzz=fuzz.__main__:main
+    '''
+)
