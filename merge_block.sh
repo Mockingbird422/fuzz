a2609d6867aef82c4b34fa30387987193e2fa06c
Andrew Marder <andrew.n.marder@gmail.com>
2017-02-21 15:55:09 -0500

    Minor edits

--- merge_block.sh
+++ merge_block.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 #
-#SBATCH -p serial_requeue  # Partition
-#SBATCH -n 1               # one core
-#SBATCH -N 1               # on one node
-#SBATCH -t 10              # Running time of 10 minutes
-#SBATCH --mem 4000         # Memory request of 4 GB
-#SBATCH --open-mode=append # Don't reset log when requeued
+#SBATCH -p hbs_rcs          # Partition
+#SBATCH -n 1                # one core
+#SBATCH -N 1                # on one node
+#SBATCH -t 7-00:00          # Running time of 7 days
+#SBATCH --mem 4000          # Memory request of 4 GB
+#SBATCH --open-mode=append  # Don't reset log when requeued
+#SBATCH --constraint=holyib # run on InfiniBand
 
 echo "$*"
 python /n/hbs_pilot/Lab/amarder/gazetteer/merge_block.py --block-id ${SLURM_ARRAY_TASK_ID} $*
